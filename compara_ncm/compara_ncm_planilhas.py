8a58ce847cbd7c3feb45cf350331f76325bed485
diogoheck <diogo.heck.dev@gmail.com>
2022-10-29 19:53:24 -0300

    Versao_3

---
+++ compara_ncm/compara_ncm_planilhas.py
@@ -0,0 +1,18 @@
+def ncm_produto_x_ncm_alteradas(NCM, lista_ncm):
+    NCM = str(NCM)
+    if NCM[0:8] in lista_ncm:
+        return True
+    elif NCM[0:7] in lista_ncm:
+        return True
+    elif NCM[0:6] in lista_ncm:
+        return True
+    elif NCM[0:5] in lista_ncm:
+        return True
+    elif NCM[0:4] in lista_ncm:
+        return True
+    elif NCM[0:3] in lista_ncm:
+        return True
+    elif NCM[0:2] in lista_ncm:
+        return True
+
+    return False
